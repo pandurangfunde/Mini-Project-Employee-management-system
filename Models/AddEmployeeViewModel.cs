865385365552e630a47a05554a2b4d9c6f2e9f45
pandurangfunde <122004953+pandurangfunde@users.noreply.github.com>
2023-03-07 13:53:01 +0530

    Add files via upload

---
+++ Models/AddEmployeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MVCCRUD.Models
+{
+    public class AddEmployeeViewModel
+    {
+       
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public double Salary { get; set; }
+
+        public DateTime DOB { get; set; }
+        public string Department { get; set; }
+    }
+}
