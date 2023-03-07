02e59becb4ec240e0c6fcc4676b42f7a9cc40562
pandurangfunde <122004953+pandurangfunde@users.noreply.github.com>
2023-03-07 13:47:49 +0530

    Add files via upload

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using MVCCRUD.Models.Domain;
+
+namespace MVCCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
